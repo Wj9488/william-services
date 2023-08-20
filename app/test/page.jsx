c6c133f6c976f32c950ed6b9a1b6107863d20e04
Will Jones <wtjones10@gmail.com>
2023-08-20 18:58:26 +0100

    swipe test

---
+++ app/test/page.jsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { motion } from "framer-motion"
+import { useState, useEffect } from "react"
+import { useRouter } from 'next/navigation';
+
+const TestPage = () => {
+    const [swipeProgress, setSwipeProgress] = useState(0)
+    const router = useRouter();
+
+    console.log(swipeProgress)
+
+    useEffect(() => {
+        if (swipeProgress >= 100) {
+            router.push('/');
+        }
+    }, [swipeProgress]);
+
+    const handleSwipe = (progress) => {
+        setSwipeProgress(progress * 100);
+    }
+
+    return (
+        <main>
+            <div className="min-h-[100dvh] min-w-[100%] overflow-x-hidden flex items-center justify-center">
+                <motion.div 
+                    drag="x" 
+                    dragConstraints={{ left: 0, right: 100 }} 
+                    onDrag={(e, info) => handleSwipe(info.point.x / window.innerWidth)}
+                    className="bg-red-500 p-4 rounded cursor-pointer"
+                >
+                    Swipe Me
+                </motion.div>
+                <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+                    <div className="h-4 bg-gray-300 rounded-full relative w-60">
+                        <motion.div 
+                            initial={{ width: 0 }}
+                            animate={{ width: `${swipeProgress}%` }}
+                            className="bg-green-500 h-4 rounded-full"
+                        ></motion.div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default TestPage
